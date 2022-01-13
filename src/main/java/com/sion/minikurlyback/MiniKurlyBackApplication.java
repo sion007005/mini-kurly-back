c7ab0a1ceef2df72fa1beeeb298fb732518e74ef
Sion <sion007005@gmail.com>
2022-01-13 23:10:19 +0900

    Initial commit

---
+++ src/main/java/com/sion/minikurlyback/MiniKurlyBackApplication.java
@@ -0,0 +1,13 @@
+package com.sion.minikurlyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniKurlyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniKurlyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sion/minikurlyback/MiniKurlyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sion.minikurlyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniKurlyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
