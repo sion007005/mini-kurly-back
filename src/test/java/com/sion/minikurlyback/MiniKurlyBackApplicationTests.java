1f3422fd06d0e6d45b24fa239b5c16cbf864368e
Sion <sion007005@gmail.com>
2022-02-02 22:16:28 +0900

    테스트 실패를 유발하는 빈 메소드 삭제

--- src/test/java/com/sion/minikurlyback/MiniKurlyBackApplicationTests.java
+++ src/test/java/com/sion/minikurlyback/MiniKurlyBackApplicationTests.java
@@ -1,13 +1,7 @@
 package com.sion.minikurlyback;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MiniKurlyBackApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
